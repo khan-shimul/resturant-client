a9685613dcc532e1a084a2659cd2c8a9e3655f7b
khan-shimul <khanshimul196@gmail.com>
2024-12-19 01:40:15 +0600

    design-add-items

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,31 +1,97 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+  const axiosPublic = useAxiosPublic();
+
+  const onSubmit = async (data) => {
     console.log(data);
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    console.log(res.data);
   };
   return (
     <div>
       <SectionTitle heading="Add Recipe" subHeading="What's New?" />
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("name")} />
-          <select
-            {...register("category")}
-            className="select select-bordered w-full max-w-xs"
-          >
-            <option disabled selected>
-              Category
-            </option>
-            <option value="salad">Salad</option>
-            <option value="pizza">Pizza</option>
-            <option value="soup">Soup</option>
-            <option value="dessert">Dessert</option>
-            <option value="drinks">Drinks</option>
-          </select>
-          <input type="submit" />
+          <label className="form-control w-full my-6">
+            <div className="label">
+              <span className="label-text">Recipe Name*</span>
+            </div>
+            <input
+              {...register("name", { required: true })}
+              type="text"
+              placeholder="recipe name"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <div className="flex gap-6">
+            <div className="w-1/2">
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text">Category*</span>
+                </div>
+                <select
+                  defaultValue="default"
+                  {...register("category", { required: true })}
+                  className="select select-bordered w-full"
+                >
+                  <option value="default" disabled>
+                    Category
+                  </option>
+                  <option value="salad">Salad</option>
+                  <option value="pizza">Pizza</option>
+                  <option value="soup">Soup</option>
+                  <option value="dessert">Dessert</option>
+                  <option value="drinks">Drinks</option>
+                </select>
+              </label>
+            </div>
+            <div className="w-1/2">
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text">Price*</span>
+                </div>
+                <input
+                  {...register("price", { required: true })}
+                  type="number"
+                  placeholder="recipe name"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+          <label className="form-control my-6">
+            <div className="label">
+              <span className="label-text">Recipe Details*</span>
+            </div>
+            <textarea
+              {...register("recipe", { required: true })}
+              className="textarea textarea-bordered h-28"
+              placeholder="recipe details"
+            ></textarea>
+          </label>
+          <div className="mb-6">
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input w-full max-w-xs"
+            />
+          </div>
+          <button className="btn btn-warning">
+            Add Item <FaUtensils className="ml-2" />
+          </button>
         </form>
       </div>
     </div>
