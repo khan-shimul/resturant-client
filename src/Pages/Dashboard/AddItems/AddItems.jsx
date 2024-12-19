f3ca2f267a40ba3b630fb108ca4ca4b8d0c35e2e
khan-shimul <khanshimul196@gmail.com>
2024-12-20 00:22:14 +0600

    implement-add-items

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -2,23 +2,59 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { useState } from "react";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
-  const axiosPublic = useAxiosPublic();
+  const { register, handleSubmit, reset } = useForm();
+  // const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = async (data) => {
-    console.log(data);
-    const imageFile = { image: data.image[0] };
-    const res = await axiosPublic.post(image_hosting_api, imageFile, {
-      headers: {
-        "content-type": "multipart/form-data",
-      },
+    // Image send to imgBB and get the url
+    // const imageFile = { image: data.image[0] };
+    // const res = await axiosPublic.post(image_hosting_api, imageFile, {
+    //   headers: {
+    //     "content-type": "multipart/form-data",
+    //   },
+    // });
+    // console.log(res.data);
+    const formData = new FormData();
+    setLoading(true);
+    formData.append("image", data.image[0]);
+    const imageRes = await fetch(image_hosting_api, {
+      method: "post",
+      body: formData,
     });
-    console.log(res.data);
+    const result = await imageRes.json();
+    if (result.success) {
+      // send the menu data to database with img url
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        recipe: data.recipe,
+        price: parseFloat(data.price),
+        image: result.data.display_url,
+      };
+      const menuRes = await axiosSecure.post("/menu", menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.insertedId) {
+        setLoading(false);
+        reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Menu Item Added Successfully",
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      }
+    }
   };
   return (
     <div>
@@ -90,7 +126,13 @@ const AddItems = () => {
             />
           </div>
           <button className="btn btn-warning">
-            Add Item <FaUtensils className="ml-2" />
+            {loading ? (
+              <span className="loading loading-spinner loading-lg"></span>
+            ) : (
+              <span className="flex items-center">
+                Add Item <FaUtensils className="ml-2" />
+              </span>
+            )}
           </button>
         </form>
       </div>
