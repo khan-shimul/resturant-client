4b68c474f40963c4c7334649f631026c8a466e05
khan-shimul <khanshimul196@gmail.com>
2024-12-27 01:01:34 +0600

    complete-app

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -87,7 +87,9 @@ export const router = createBrowserRouter([
           </AdminRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/menu/${params.id}`),
+          fetch(
+            `https://bistro-boss-server-three-livid.vercel.app/menu/${params.id}`
+          ),
       },
       {
         path: "users",
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -8,7 +8,7 @@ export const useMenu = () => {
   const axiosPublic = useAxiosPublic();
 
   // useEffect(() => {
-  //   fetch("http://localhost:5000/menu")
+  //   fetch("https://bistro-boss-server-three-livid.vercel.app/menu")
   //     .then((res) => res.json())
   //     .then((data) => {
   //       setMenu(data);
